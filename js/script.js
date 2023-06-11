7769f3e9ce267c92ec454e52204219e15de41af7
chalund <114383740+chalund@users.noreply.github.com>
2023-06-11 19:25:31 +0200

    cleaning code

--- js/script.js
+++ js/script.js
@@ -2,25 +2,17 @@ const aboutSection = document.querySelector(".about");
 const workSection = document.querySelector(".project");
 const contact = document.querySelector(".contact_me");
 
-
 function scrollToSection(about) {
-    // Retrieve the section element using the provided sectionClass
     const section = document.querySelector(about);
-    // Scroll to the section smoothly
     section.scrollIntoView({ behavior: 'smooth' });
 }
 
 function scrollToSection(project) {
-    // Retrieve the section element using the provided sectionClass
     const section = document.querySelector(project);
-    // Scroll to the section smoothly
     section.scrollIntoView({ behavior: 'smooth' });
 }
 
 function scrollToSection(contact_me) {
-    // Retrieve the section element using the provided sectionClass
     const section = document.querySelector(contact_me);
-    // Scroll to the section smoothly
     section.scrollIntoView({ behavior: 'smooth' });
 }
-
